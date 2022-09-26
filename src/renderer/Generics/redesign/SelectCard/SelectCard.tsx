935f1e1f277841a8337c5c999e04ab7ac5bc9c92
jgresham <johnsgresham@gmail.com>
2022-09-26 11:33:45 -0700

    fix: call click prop. lint.

--- src/renderer/Generics/redesign/SelectCard/SelectCard.tsx
+++ src/renderer/Generics/redesign/SelectCard/SelectCard.tsx
@@ -1,7 +1,15 @@
+/* eslint-disable jsx-a11y/click-events-have-key-events */
 import { NodeIconId } from 'renderer/assets/images/nodeIcons';
 import { NodeIcon } from '../NodeIcon';
 import { Tag } from '../Tag';
-import { container, descriptionStyle, selectedContainer, tagStyle, textContainer, titleStyle } from './selectCard.css';
+import {
+  container,
+  descriptionStyle,
+  selectedContainer,
+  tagStyle,
+  textContainer,
+  titleStyle,
+} from './selectCard.css';
 
 export interface SelectCardProps {
   /**
@@ -39,25 +47,33 @@ const SelectCard = ({
   info,
   iconId,
   minority = false,
-  isSelected = false
+  isSelected,
 }: SelectCardProps) => {
-
-  const containerStyles = [container]
-  if(isSelected) {
-    containerStyles.push(selectedContainer)
+  const containerStyles = [container];
+  if (isSelected) {
+    containerStyles.push(selectedContainer);
   }
   return (
-    // eslint-disable-next-line jsx-a11y/click-events-have-key-events
-    <div className={containerStyles.join(" ")}>
+    <div
+      className={containerStyles.join(' ')}
+      onClick={() => {
+        if (onClick) {
+          onClick();
+        }
+      }}
+    >
       <NodeIcon iconId={iconId} size="medium" />
       <div className={textContainer}>
         {/* TODO: Fix height to 60px */}
         <div className={titleStyle}>{title}</div>
         <div className={descriptionStyle}>{info}</div>
       </div>
-      {minority && <div className={tagStyle}><Tag  type="pink" label="Minority Client" />    </div>
-}
+      {minority && (
+        <div className={tagStyle}>
+          <Tag type="pink" label="Minority Client" />{' '}
+        </div>
+      )}
     </div>
   );
 };
-export default SelectCard
+export default SelectCard;
