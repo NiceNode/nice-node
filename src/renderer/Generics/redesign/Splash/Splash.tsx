c9c847fa42a4038a4eced466cd8a0681d24947bb
corn-potage <cornpotage@protonmail.com>
2022-10-18 15:58:00 -0700

    added onboarding images, modified splash

--- src/renderer/Generics/redesign/Splash/Splash.tsx
+++ src/renderer/Generics/redesign/Splash/Splash.tsx
@@ -1,4 +1,10 @@
-import { container, titleFont, descriptionFont, iconClass } from './splash.css';
+import {
+  container,
+  contentContainer,
+  titleFont,
+  descriptionFont,
+  iconClass,
+} from './splash.css';
 import icon from '../../../../../assets/icon.png';
 import Button from '../Button/Button';
 
@@ -8,16 +14,22 @@ export interface SplashProps {
 }
 
 const Splash = ({ title, description }: SplashProps) => {
+  const welcomeImage = require('../../../assets/images/artwork/welcome.png');
   return (
-    <div className={container}>
-      <img className={iconClass} alt="NiceNode logo" src={icon} />
-      <div className={titleFont}>{title}</div>
-      <div className={descriptionFont}>{description}</div>
-      <Button
-        label="Get started"
-        primary
-        onClick={() => console.log('Get started')}
-      />
+    <div
+      style={{ backgroundImage: `url(${welcomeImage})` }}
+      className={container}
+    >
+      <div className={contentContainer}>
+        <img className={iconClass} alt="NiceNode logo" src={icon} />
+        <div className={titleFont}>{title}</div>
+        <div className={descriptionFont}>{description}</div>
+        <Button
+          label="Get started"
+          primary
+          onClick={() => console.log('Get started')}
+        />
+      </div>
     </div>
   );
 };
