e02bc748adf0727eb8a0a87c1afd082a9608b841
corn-potage <cornpotage@protonmail.com>
2022-10-26 17:29:13 -0700

    now able to configure content in stories

--- src/stories/Presentational/ContentMultipleClients.stories.tsx
+++ src/stories/Presentational/ContentMultipleClients.stories.tsx
@@ -13,4 +13,53 @@ const Template: ComponentStory<typeof ContentMultipleClients> = (args) => (
 );
 
 export const Primary = Template.bind({});
-Primary.args = {};
+Primary.args = {
+  clients: [
+    {
+      name: 'nimbus',
+      version: 'v10',
+      type: 'single',
+      nodeType: 'consensus',
+      status: {
+        initialized: true,
+        synchronized: true,
+        synchronizing: 99,
+        lowPeerCount: false,
+        updateAvailable: false,
+        blocksBehind: true,
+        noConnection: false,
+        stopped: false,
+        error: false,
+      },
+      stats: {
+        peers: 20,
+        slot: '4,456,158',
+        cpuLoad: 20,
+        diskUsage: 600, // in MB?
+      },
+    },
+    {
+      name: 'besu',
+      version: 'v10',
+      type: 'single',
+      nodeType: 'execution',
+      status: {
+        initialized: true,
+        synchronizing: 99,
+        synchronized: true,
+        lowPeerCount: false,
+        updateAvailable: true,
+        blocksBehind: true,
+        noConnection: false,
+        stopped: false,
+        error: false,
+      },
+      stats: {
+        peers: 16,
+        block: '15791798',
+        cpuLoad: 82,
+        diskUsage: 5000,
+      },
+    },
+  ],
+};
