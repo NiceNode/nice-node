eb0cb2e669fd9f519c8edbb8cca7ed7e5027eb4a
jgresham <johnsgresham@gmail.com>
2023-03-13 14:06:19 -0700

    chore: hide network select on add node

--- src/renderer/Presentational/AddEthereumNode/AddEthereumNode.tsx
+++ src/renderer/Presentational/AddEthereumNode/AddEthereumNode.tsx
@@ -16,8 +16,8 @@ import SpecialSelect, {
   SelectOption,
 } from '../../Generics/redesign/SpecialSelect/SpecialSelect';
 import electron from '../../electronGlobal';
-import DropdownLink from '../../Generics/redesign/Link/DropdownLink';
-import Select from '../../Generics/redesign/Select/Select';
+// import DropdownLink from '../../Generics/redesign/Link/DropdownLink';
+// import Select from '../../Generics/redesign/Select/Select';
 // import { NodeSpecification } from '../../../common/nodeSpec';
 import FolderInput from '../../Generics/redesign/Input/FolderInput';
 import { HorizontalLine } from '../../Generics/redesign/HorizontalLine/HorizontalLine';
@@ -135,7 +135,7 @@ const AddEthereumNode = ({
 AddEthereumNodeProps) => {
   const { t } = useTranslation();
   const { t: tGeneric } = useTranslation('genericComponents');
-  const [sIsOptionsOpen, setIsOptionsOpen] = useState<boolean>();
+  // const [sIsOptionsOpen, setIsOptionsOpen] = useState<boolean>();
   const [sSelectedExecutionClient, setSelectedExecutionClient] =
     useState<SelectOption>(ethereumNodeConfig?.executionClient || ecOptions[0]);
   const [sSelectedConsensusClient, setSelectedConsensusClient] =
@@ -252,7 +252,8 @@ AddEthereumNodeProps) => {
         onChange={onChangeCc}
         options={ccOptions}
       />
-      <DropdownLink
+      {/* Todo: add and make work */}
+      {/* <DropdownLink
         text={`${
           sIsOptionsOpen
             ? tGeneric('HideAdvancedOptions')
@@ -288,7 +289,7 @@ AddEthereumNodeProps) => {
             />
           </div>
         </div>
-      )}
+      )} */}
       <HorizontalLine />
       <p className={sectionFont}>{tGeneric('DataLocation')}</p>
       <FolderInput
