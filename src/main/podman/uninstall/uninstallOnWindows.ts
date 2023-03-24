79dfd696dd63c90d6e0716a40492bfef2e812686
jgresham <johnsgresham@gmail.com>
2023-03-23 23:24:32 -0700

    feat: uninstall podman and remove podman files for windows. untested thoroughly

--- src/main/podman/uninstall/uninstallOnWindows.ts
+++ src/main/podman/uninstall/uninstallOnWindows.ts
@@ -1,3 +1,4 @@
+import { app } from 'electron';
 import logger from '../../logger';
 import { execAwait } from '../../execHelper';
 
@@ -16,16 +17,39 @@ const uninstallOnWindows = async (
     let stdout;
     let stderr;
 
+    // may need to run wsl -t podman-nicenode-machine and wsl --unregister podman-nicenode-machine
+    // Returns C:\Users\<user> (Ex. C:\Users\johns)
+    const userHome = app.getPath('home');
+    const foldersToDelete = [
+      `${userHome}/.local/share/containers/podman`,
+      `${userHome}/.config/containers/podman`,
+      `${userHome}/.ssh/*podman*`,
+      `${userHome}/.ssh/*nicenode*`,
+    ];
+    foldersToDelete.forEach(async (folderPath) => {
+      const rmCommand = `Remove-Item -Path ${folderPath} -Recurse -Force`;
+      ({ stdout, stderr } = await execAwait(rmCommand, {
+        log: true,
+      }));
+      logger.info(`podman uninstall rm ${folderPath} ${stdout}, ${stderr}`);
+    });
+
     // This uninstall command is equivalent to the user going to Add & Remove programs UI
     // and clicking Uninstall. Command requires sudo.
-    // todo: query for the podman version
-    const uninstallCommand = `(Get-WmiObject -Class Win32_Product -Filter "Name = 'Podman ${version}'").Uninstall()`;
-    // eslint-disable-next-line prefer-const
+    // get the name of the application
+    const getPodmanAppName = `Get-WmiObject -Class Win32_Product -Filter "Name LIKE '%Podman%'" | Select-Object -ExpandProperty Name`;
+    ({ stdout, stderr } = await execAwait(getPodmanAppName, {
+      log: true,
+    }));
+    const appName = stdout;
+    logger.info(`podman uninstall getPodmanAppName ${stdout}, ${stderr}`);
+
+    const uninstallCommand = `(Get-WmiObject -Class Win32_Product -Filter "Name = '${appName}'").Uninstall()`;
     ({ stdout, stderr } = await execAwait(uninstallCommand, {
       log: true,
       sudo: true,
     }));
-    console.log('podman uninstall stdout, stderr', stdout, stderr);
+    logger.info(`podman uninstall app ${stdout}, ${stderr}`);
 
     return true;
     // eslint-disable-next-line @typescript-eslint/no-explicit-any
