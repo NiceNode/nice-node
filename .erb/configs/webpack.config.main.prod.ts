18d6290f3f9cf2bd0f5329e773edee326205ebdc
jgresham <johnsgresham@gmail.com>
2023-04-03 13:58:36 -0700

    fix local packaging. remove explicit background tiff for dmg background

--- .erb/configs/webpack.config.main.prod.ts
+++ .erb/configs/webpack.config.main.prod.ts
@@ -14,6 +14,7 @@ import deleteSourceMaps from '../scripts/delete-source-maps';
 
 checkNodeEnv('production');
 deleteSourceMaps();
+require('dotenv').config();
 
 const devtoolsConfig =
   process.env.DEBUG_PROD === 'true'
