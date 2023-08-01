8e518df75fffa575ac200c9404fba0c8035a44f3
jgresham <johnsgresham@gmail.com>
2023-08-01 16:07:55 -0700

    fix for delete source maps script path on windows

--- .erb/scripts/delete-source-maps.js
+++ .erb/scripts/delete-source-maps.js
@@ -1,8 +1,15 @@
+import fs from 'fs';
 import path from 'path';
 import { rimrafSync } from 'rimraf';
 import webpackPaths from '../configs/webpack.paths';
 
 export default function deleteSourceMaps() {
-  rimrafSync(path.join(webpackPaths.distMainPath, '*.js.map'));
-  rimrafSync(path.join(webpackPaths.distRendererPath, '*.js.map'));
+  if (fs.existsSync(webpackPaths.distMainPath))
+    rimrafSync(path.join(webpackPaths.distMainPath, '*.js.map'), {
+      glob: true,
+    });
+  if (fs.existsSync(webpackPaths.distRendererPath))
+    rimrafSync(path.join(webpackPaths.distRendererPath, '*.js.map'), {
+      glob: true,
+    });
 }
