3f60a0765d72b80e2a6ee48685fedd87dab76637
Gathin <94786511+Gathin23@users.noreply.github.com>
2023-10-23 08:54:34 +0530

    Minor redesign for copy buttons (#350)
    * Add: CopyButton(with base css)
    
    * Add: Icon with hover change
    
    * Add: Copy feature for Wallet connections RPC

--- src/renderer/Generics/redesign/LogMessage/LogMessage.tsx
+++ src/renderer/Generics/redesign/LogMessage/LogMessage.tsx
@@ -6,7 +6,7 @@ import {
   messageStyle,
   copyStyle,
 } from './logMessage.css';
-import FloatingButton from '../FloatingButton/FloatingButton';
+import CopyButton from '../CopyButton/CopyButton';
 
 export interface LogMessageProps {
   /**
@@ -24,10 +24,6 @@ export interface LogMessageProps {
 }
 
 export const LogMessage = ({ timestamp, level, message }: LogMessageProps) => {
-  const copy = async () => {
-    await navigator.clipboard.writeText(message);
-  };
-
   return (
     <div className={container}>
       <div className={timestampStyle}>
@@ -38,7 +34,7 @@ export const LogMessage = ({ timestamp, level, message }: LogMessageProps) => {
       <div className={[levelStyle, `${level}`].join(' ')}>{level}</div>
       <div className={messageStyle}>{message}</div>
       <div className={copyStyle}>
-        <FloatingButton variant="icon" iconId="copy" onClick={copy} />
+        <CopyButton data={message} />
       </div>
     </div>
   );
