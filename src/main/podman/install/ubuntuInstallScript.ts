732649c4c369ca3c852274cac481ae59a3e3b921
jgresham <johnsgresham@gmail.com>
2023-10-24 03:11:30 -0700

    fix: ubuntu install script

--- src/main/podman/install/ubuntuInstallScript.ts
+++ src/main/podman/install/ubuntuInstallScript.ts
@@ -3,12 +3,12 @@
 export const script = `apt-get -y update -qq
 apt-get -y -qq install curl
 mkdir -p /etc/apt/keyrings
-curl -fsSL https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/Release.key \
+curl -fsSL https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_22.04/Release.key \
   | gpg --dearmor \
   | tee /etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg > /dev/null
 echo \
   "deb [arch=$(dpkg --print-architecture) signed-by=/etc/apt/keyrings/devel_kubic_libcontainers_unstable.gpg]\
-    https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_$(lsb_release -rs)/ /" \
+    https://download.opensuse.org/repositories/devel:kubic:libcontainers:unstable/xUbuntu_22.04/ /" \
   | tee /etc/apt/sources.list.d/devel:kubic:libcontainers:unstable.list > /dev/null
 apt-get update -qq
 apt-get -qq -y install podman`;
