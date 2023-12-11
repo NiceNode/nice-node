813a442a5186a8b1c0b3ad0f9d10b2a5fbdc01bc
jgresham <johnsgresham@gmail.com>
2023-12-11 15:58:13 -0600

    try all. known that linux runs but fails due to electron upgrade

--- wdio.conf.ts
+++ wdio.conf.ts
@@ -68,7 +68,9 @@ export const config: Options.Testrunner = {
         // custom application args
         appArgs: [],
         appBinaryPath:
-          '/home/runner/work/nice-node/nice-node/release/build/linux-unpacked/nice-node',
+          process.platform === 'linux'
+            ? './release/build/linux-unpacked/nice-node'
+            : undefined,
       },
     },
   ],
