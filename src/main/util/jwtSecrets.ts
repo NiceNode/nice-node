f3e216d21e39e598e1a0184b74b2dda28ce6525f
jgresham <johnsgresham@gmail.com>
2024-02-09 09:59:28 -0800

    use jwt-file flag for hildr. alc sep urls for testing

--- src/main/util/jwtSecrets.ts
+++ src/main/util/jwtSecrets.ts
@@ -15,9 +15,7 @@ import crypto from 'node:crypto';
 export const createJwtSecretAtDirs = async (
   dirPaths: string[],
 ): Promise<boolean> => {
-  // const jwtSecret = `0x${crypto.randomBytes(32).toString('hex')}`;
-  const jwtSecret =
-    'bf549f5188556ce0951048ef467ec93067bc4ea21acebe46ef675cd4e8e015ff';
+  const jwtSecret = `0x${crypto.randomBytes(32).toString('hex')}`;
   // dirPaths.forEach(async (dirPath) => {
   for (const dirPath of dirPaths) {
     // open file for writing only, fails if the file exists
