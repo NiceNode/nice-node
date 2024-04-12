9a34d5abbc54b6137ffb73771e908ef01b2894d5
jgresham <johnsgresham@gmail.com>
2024-04-11 18:54:02 -0700

    try fixing linux icon - still not in deb

--- forge.config.ts
+++ forge.config.ts
@@ -7,10 +7,11 @@ import { MakerDMG } from '@electron-forge/maker-dmg';
 import { VitePlugin } from '@electron-forge/plugin-vite';
 import { FusesPlugin } from '@electron-forge/plugin-fuses';
 import { FuseV1Options, FuseVersion } from '@electron/fuses';
+import * as path from 'node:path';
 
 const packagerConfig: ForgePackagerOptions = {
   asar: true,
-  icon: './assets/icon',
+  icon: './assets/icon.png',
   executableName: 'nice-node', // required for linux?
   // unsure if this is needed below:
   // ignore: [ /stories/, /__tests__/, /.storybook/, /storybook/, /storybook-static/ ],
@@ -33,6 +34,9 @@ if(process.env.CI) {
   };
 }
 
+const iconDir = path.resolve(__dirname, 'assets', 'icons');
+console.log("forge.config.ts iconDir: ", iconDir);
+
 const config: ForgeConfig = {
   packagerConfig,
 
@@ -41,9 +45,41 @@ const config: ForgeConfig = {
     new MakerSquirrel({
       authors: 'NiceNode LLC',
     }, ['windows']),
-    new MakerZIP({}),
-    new MakerRpm({}, ['linux']),
-    new MakerDeb({}, ['linux']),
+    // new MakerZIP({}),
+    // new MakerRpm({}, ['linux']),
+    // new MakerDeb({
+    //     // icon: {
+    //     //   scalable: './assets/icons/icon.svg',
+    //     //   '1024x1024': './assets/icons/1024x1024.png',
+    //     //   // 256x256 is the largest icon size that will be displayed in the Ubuntu Software Center
+    //     //   '256x256': './assets/icons/256x256.png',
+    //     // },
+    //     icon: './assets/icons/icon.svg',
+    //     productName: 'NiceNode',
+    //     productDescription: "By running a node you become part of a global movement to decentralize a world of information. Prevent leaking your personal data to third party nodes. Ensure access when you need it, and don't be censored. Decentralization starts with you. Voice your choice, help your peers.",
+    //     maintainer: "NiceNode LLC <johns@nicenode.xyz>",
+    //     categories: ['Utility', 'System', 'Network', 'Development'],
+    // }, ['linux']),
+    {
+      name: '@electron-forge/maker-deb',
+      platforms: ['linux'],
+      config: {
+        icon: {
+          scalable: path.resolve(iconDir, 'icon.svg'),
+          // scalable: './assets/icons/icon.svg',
+          '1024x1024': path.resolve(iconDir, '1024x1024.png'),
+          // '1024x1024': './assets/icons/1024x1024.png',
+          // 256x256 is the largest icon size that will be displayed in the Ubuntu Software Center
+          '256x256': path.resolve(iconDir, '256x256.png'),
+          // '256x256': './assets/icons/256x256.png',
+        },
+        // icon: './assets/icons/icon.svg',
+        productName: 'NiceNode',
+        productDescription: "By running a node you become part of a global movement to decentralize a world of information. Prevent leaking your personal data to third party nodes. Ensure access when you need it, and don't be censored. Decentralization starts with you. Voice your choice, help your peers.",
+        maintainer: "NiceNode LLC <johns@nicenode.xyz>",
+        categories: ['Utility', 'System', 'Network', 'Development'],
+    }
+    },
     new MakerDMG({
       background: './assets/dmg-background.tiff',
       // installer name. default includes version number in filename
