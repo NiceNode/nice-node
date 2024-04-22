420680f09dcf7dc052a59d82b4c1796c7c5ef669
jgresham <johnsgresham@gmail.com>
2024-04-22 10:41:37 -0700

    bump 5.5.2. remove electron-updater reference code

--- src/main/electron-updater/differentialDownloader/GenericDifferentialDownloader.ts
+++
@@ -1,8 +0,0 @@
-import { BlockMap } from "builder-util-runtime/out/blockMapApi"
-import { DifferentialDownloader } from "./DifferentialDownloader"
-
-export class GenericDifferentialDownloader extends DifferentialDownloader {
-  download(oldBlockMap: BlockMap, newBlockMap: BlockMap): Promise<any> {
-    return this.doDownload(oldBlockMap, newBlockMap)
-  }
-}
