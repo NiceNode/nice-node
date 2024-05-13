1c57b57e8d35a1a64668b9ff0b480168f5f181a4
corn-potage <cornpotage@protonmail.com>
2024-05-13 14:23:47 -0700

    added alert dialog

--- src/main/tray.ts
+++ src/main/tray.ts
@@ -1,4 +1,4 @@
-import { Menu, MenuItem, Tray } from 'electron';
+import { Menu, MenuItem, Tray, dialog } from 'electron';
 import logger from './logger';
 import { createWindow, fullQuit, getMainWindow } from './main';
 import { isLinux, isWindows } from './platform';
@@ -61,8 +61,24 @@ export const setTrayMenu = () => {
     {
       label: 'Quit',
       click: () => {
-        fullQuit(); // app no longer runs in the background
-      },
+        // Show confirmation dialog before quitting
+        // TODO: get translated strings for this
+        dialog.showMessageBox({
+          type: 'question',
+          buttons: ['Yes', 'No'],
+          defaultId: 1, // Focus on 'No' by default
+          title: 'Confirm',
+          message: 'Are you sure you want to quit? Nodes will stop syncing.',
+          detail: 'Confirming will close the application.'
+        }).then(result => {
+          if (result.response === 0) { // The 'Yes' button is at index 0
+            fullQuit(); // app no longer runs in the background
+          }
+          // Do nothing if the user selects 'No'
+        }).catch(err => {
+          logger.error('Error showing dialog:', err);
+        });
+      }
     },
   ];
   const contextMenu = Menu.buildFromTemplate(menuTemplate as MenuItem[]);
