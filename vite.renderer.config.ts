ce15597aabd29abdcf1462d8de22a693ca0200ac
jgresham <johnsgresham@gmail.com>
2024-05-13 14:28:17 -0700

    fix: event reporting env vars

--- vite.renderer.config.ts
+++ vite.renderer.config.ts
@@ -2,8 +2,11 @@ import type { ConfigEnv, PluginOption, UserConfig } from 'vite';
 import { defineConfig } from 'vite';
 import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin';
 import svgr from "vite-plugin-svgr";
-
+import dotenv from 'dotenv';
 import { pluginExposeRenderer } from './vite.base.config.js';
+
+// applies env vars from .env file(s)
+dotenv.config();
 
 console.log("vite.renderer.config.ts");
 
@@ -33,5 +36,11 @@ export default defineConfig((env) => {
       preserveSymlinks: true,
     },
     clearScreen: false,
+    define: {
+      'import.meta.env.MP_PROJECT_TOKEN': JSON.stringify(process.env.MP_PROJECT_TOKEN),
+      'import.meta.env.MP_PROJECT_ENV': JSON.stringify(process.env.MP_PROJECT_ENV),
+      'import.meta.env.NICENODE_ENV': JSON.stringify(process.env.NICENODE_ENV),
+      'import.meta.env.SENTRY_DSN': JSON.stringify(process.env.SENTRY_DSN),
+    }
   } as UserConfig;
 });
